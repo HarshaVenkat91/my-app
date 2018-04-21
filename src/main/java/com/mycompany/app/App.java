71f0ad554a630af3971ef207d8aebe3c09b76df3
HarshaVenkat91 <harshavenkat1991@gmail.com>
2018-04-21 21:02:03 +0000

    change integration - 1

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "GIT HOOK trigger build- this time with integration and services" );
+        System.out.println( "GIT HOOK trigger build- this time with integration and services - again" );
     }
 }
